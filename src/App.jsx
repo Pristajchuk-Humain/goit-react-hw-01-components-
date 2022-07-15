04c4b0e870b9c72eb53bddbd84460a717631c286
Pristajchuk-Humain <104837383+Pristajchuk-Humain@users.noreply.github.com>
2022-07-15 10:43:35 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -17,18 +17,16 @@ function App() {
   return (
     <>
     <User 
-   username={user.username}
+  username={user.username}
   tag={user.tag}
   location={user.location}
   avatar={user.avatar}
   stats={user.stats}
 />
-    <Statistics title="Upload stats" stats={data} />;
-    <Statistics stats={data} />;
-
-    <FriendList friends={friends} />, 
-
-  <TransactionHistory items={transactions} />;  
+    <Statistics title="Upload stats" stats={data} />
+    <Statistics stats={data} />
+    <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />
     </>
   )
 }
